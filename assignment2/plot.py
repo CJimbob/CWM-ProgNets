08258630dce6798e8083ebcf953176f48d2d3ef1
Live session user <zhaozhouc@gmail.com>
2025-06-10 12:09:40 +0100

    assignment finished

--- assignment2/plot.py
+++ assignment2/plot.py
@@ -3,20 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # parameters to modify
-filename="ping.log5"
-label='ping'
-xlabel = 'xlabel'
-ylabel = 'time'
-title='Ping Test5, Interval 0.001'
+filename="iperf10.log"
+label= ''
+xlabel = 'time (sec)'
+ylabel = '%packets lost'
+title='Iperf6 bandwidth = 100m'
 fig_name='test.png'
 bins=100 #adjust the number of bins to your plot
 
 
 t = np.loadtxt(filename, delimiter=" ", dtype="float")
 
-#plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
+plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
 #Comment the line above and uncomment the line below to plot a CDF
-plt.hist(t[:,1], bins, density=True, histtype='step', cumulative=True, label=label)
+#plt.hist(t[:,1], bins, density=True, histtype='step', cumulative=True, label=label)
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
 plt.title(title)
